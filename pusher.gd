7bad80107a75d169a414c8b350c1fd644dc05238
Samiser <github@me.samiser.xyz>
2025-07-03 18:18:35 +0000

    added pusher

---
+++ pusher.gd
@@ -0,0 +1,16 @@
+extends StaticBody3D
+
+var start_position: Vector3
+var frequency := 0.3
+var amplitude := 0.4
+var axis := Vector3.FORWARD
+var time_passed := 0.0
+
+func _ready() -> void:
+	start_position = global_position
+
+func _physics_process(delta: float) -> void:
+	time_passed += delta
+	var offset = amplitude * sin(time_passed * frequency * TAU)
+	global_position = start_position + axis * offset
+	
