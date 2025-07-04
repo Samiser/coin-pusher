23f78b52cb53465444e202df7dbfbc1a644e3213
Samiser <github@me.samiser.xyz>
2025-07-04 12:00:33 +0000

    refactored some machine-related code and nodes out of main

--- Objects/Machines/FirstMachine/machine.gd
+++ Objects/Machines/FirstMachine/machine.gd
@@ -2,23 +2,14 @@ extends Node3D
 
 @onready var drop_locations := $DropLocations
 @onready var coin_detector: Area3D = $CoinDetector
-@onready var coin_label: RichTextLabel
 
-var coins := 10
+signal coin_collected(value: int)
 
 func _ready() -> void:
-	var parent_node: Node3D = get_parent_node_3d()
-	coin_label = parent_node.get_node("CanvasLayer/coin_label") # this feels stinky
-	update_coin_count(0)
-	
 	coin_detector.connect("body_entered", _coin_detected)
 
 func get_random_drop_location() -> Vector3:
 	return drop_locations.get_children().pick_random().global_position
 
 func _coin_detected(body:Node3D) -> void:
-	update_coin_count(1)
-
-func update_coin_count(change:int) -> void:
-	coins += change
-	coin_label.set_text(str("Coins: ", coins))
+	emit_signal("coin_collected", 1)
--- main.gd
+++ main.gd
@@ -2,12 +2,25 @@ extends Node3D
 
 @export var coin_scene: PackedScene
 @onready var machine := $Machine
-	
+@onready var coin_label := $CanvasLayer/coin_label
+
+var coins := 10:
+	set(value):
+		coin_label.set_text(str("Coins: ", value))
+		coins = value
+
+func _ready() -> void:
+	machine.connect("coin_collected", update_coin_count)
+	coin_label.set_text(str("Coins: ", coins))
+
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("drop") && machine.coins > 0:
+	if Input.is_action_just_pressed("drop") && coins > 0:
 		var coin := coin_scene.instantiate()
 		add_child(coin)
 		coin.rotation_degrees = Vector3(90, 0, 0)
 		coin.position = machine.get_random_drop_location()
-		
-		machine.update_coin_count(-1)
+
+		coins -= 1
+
+func update_coin_count(value: int) -> void:
+	coins += value
