76e177a6db4ebe7065b34bb7199cb2071c16901d
wookbee <159189510+wookbee@users.noreply.github.com>
2025-07-04 17:16:57 +0100

    added more pinball stuff

---
+++ Objects/MachineComponents/Bumper/bumper.gd
@@ -0,0 +1,3 @@
+extends Node3D
+
+@onready var collision_shape := $StaticBody3D/CollisionShape3D
--- Objects/Machines/PinballMachine/Flipper/flipper.gd
+++ Objects/Machines/PinballMachine/Flipper/flipper.gd
@@ -3,16 +3,22 @@ extends Node3D
 @onready var flipper_body := $AnimatedRigidBody3D
 var flip_speed := 24
 var flipping := false
+var tween : Tween
+
+func _flip() -> void:
+	flipping = true
+	
+	if(tween):
+		tween.kill()
+	
+	tween = get_tree().create_tween()
+	tween.tween_property(flipper_body, "rotation_degrees:z", -32, 0.2).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(flipper_body, "rotation_degrees:z", 24, 0.2).set_trans(Tween.TRANS_SPRING)
+	#tween.tween_callback(flipper_body.queue_free)
+	
+	flipping = false
 
 # might want to use a tween for this https://docs.godotengine.org/en/stable/classes/class_tween.html
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("flip") && !flipping:
-		flipping = true;
-
-	if flipping:
-		flipper_body.rotate_z(-flip_speed * delta)
-		if(flipper_body.rotation_degrees.z < -20):
-			flipping = false
-	else:
-		if(flipper_body.rotation_degrees.z < 45):
-			flipper_body.rotate_z(flip_speed * delta)
+		_flip()
