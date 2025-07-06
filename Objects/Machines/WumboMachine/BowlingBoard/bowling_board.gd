5eb84923f26b56aa6a810298ed18465da4372680
Samiser <github@me.samiser.xyz>
2025-07-06 17:09:04 +0000

    fixed strike logic

--- Objects/Machines/WumboMachine/BowlingBoard/bowling_board.gd
+++ Objects/Machines/WumboMachine/BowlingBoard/bowling_board.gd
@@ -13,13 +13,13 @@ func _flash_and_reset_all_pins() -> void:
 func _on_pin_hit(_pin: Pin) -> void:
 	var all_pins_hit := pins.get_children().all(func(pin: Pin) -> bool: return pin.is_hit)
 	if all_pins_hit:
-		await get_tree().create_timer(1)
+		_flash_and_reset_all_pins()
 		bowling_ball.freeze = false
 		bowling_ball.position = Vector3(-0.6, 2, 0.1)
 		bowling_ball.linear_velocity = Vector3(3, -10, 0)
 		emit_signal("add_combo", 1)
 		strike_audio.play()
-		
+		await get_tree().create_timer(1)
 		
 
 func _ready() -> void:
