7dd87cb639d3a95b81fa5e424b67c2a1792045d5
wookbee <159189510+wookbee@users.noreply.github.com>
2025-07-20 17:40:41 +0100

    updated coin window

--- UI/popup_window_coin.gd
+++ UI/popup_window_coin.gd
@@ -1,7 +1,7 @@
-extends TextureRect # should add base window script eventually
+extends Window
+class_name CoinPopup
 
 @onready var window_title : RichTextLabel = $title
-@onready var window_close_button := $close_button
 
 @onready var coin_info := $info
 @onready var coin_height_slider := $slider_bg/height_slider
@@ -10,46 +10,32 @@ extends TextureRect # should add base window script eventually
 
 var coin : Coin
 
-var dragging := false
-var highlighted := false
-
 func _ready() -> void:
-	await get_tree().process_frame
+	await get_tree().process_frame # coin is set a frame late
 	
 	coin_height_slider.min_value = 0
 	coin_height_slider.max_value = coin.spawn_height
 	window_title.text = "Coin (" + str(coin.id) + ")"
+	
+	var tween := get_tree().create_tween()
+	tween.tween_property(window_title, "visible_ratio", 1, 0.4).from(0.0) 
 
 func _process(delta: float) -> void:
 	if !is_instance_valid(coin):
 		queue_free()
 		return
-	
+
 	coin_cam.global_position.x = coin.global_position.x
 	coin_cam.global_position.y = coin.global_position.y
 	
-	var coin_alive_time := Time.get_ticks_msec() - coin.spawn_time
-	coin_info.text = "Location\n" + str(coin_alive_time) + "t"
+	var coin_alive_time := (Time.get_ticks_msec() - coin.spawn_time) / 1000
+	coin_info.text = "Location\n" + str(coin_alive_time) + "s"
 	
 	coin_height_slider.value = coin.global_position.y
 		
-	if Input.is_action_pressed("click"):
-		if highlighted:
-			global_position = get_global_mouse_position()
-		
-func _on_area_2d_mouse_entered() -> void:
-	highlighted = true
-	print("window hightlighted")
-
-func _on_area_2d_mouse_exited() -> void:
-	if !Input.is_action_pressed("click"):
-		highlighted = false
-
-func _on_close_button_pressed() -> void:
-	var tween := get_tree().create_tween()
-	tween.tween_property(self, "global_position:y", 1200, 0.1)
-	await tween.finished
-	queue_free()
-
 func _on_track_button_pressed() -> void:
 	pass # Replace with function body.
+
+
+func _on_close_requested() -> void:
+	queue_free()
--- UI/ui.gd
+++ UI/ui.gd
@@ -31,8 +31,6 @@ var default_frame_tex := preload("res://UI/Sprites/ui_board_frame.png")
 
 var tween : Tween
 
-var popup_windows_count := 0
-
 signal drop_triggered
 signal swap_boards
 signal add_board
@@ -121,16 +119,8 @@ func _ToggleShop() -> void:
 	shop_panel.visible = !shop_panel.visible
 	
 func Create_Coin_Window(coin: Coin) -> void:
-	if popup_windows_count > 6:
-		return
-		
-	popup_windows_count += 1
-	
-	var popup := coin_window.instantiate()
+	var popup : CoinPopup = coin_window.instantiate()
 	add_child(popup)
-	popup.global_position.y += 32 * popup_windows_count
-	popup.global_position.x += 32 * popup_windows_count
-	popup.window_close_button.pressed.connect(func() -> void: popup_windows_count -= 1)
 	
 	popup.coin = coin
 	print("creating coin window, " + popup.name)
